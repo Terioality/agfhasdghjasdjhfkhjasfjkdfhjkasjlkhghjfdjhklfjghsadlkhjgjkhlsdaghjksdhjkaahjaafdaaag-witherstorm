5748febefbc972127731074c6bcf1fc0dc41783b
Terio <123908288+Terioality@users.noreply.github.com>
2023-04-08 15:53:20 +0200

    Update Control.lua

--- Control.lua
+++ Control.lua
@@ -2,7 +2,7 @@ local playerfold = Instance.new("Folder")
 playerfold.Name = "PlayerFolder"
 playerfold.Parent = game.Workspace
 game.Players.LocalPlayer.Character.Parent = playerfold
-game.Players.LocalPlayer.Character.Humanoid.HipHeight = 250
+game.Players.LocalPlayer.Character.Humanoid.HipHeight = 400
 	workspace.CurrentCamera:remove()
 local speaker = game.Players.LocalPlayer
 	wait(.1)
